fc9a4feef879901c029b1957666d0e451e5408d2
Renan Azeredo <renanufrpe@gmail.com>
2015-04-08 09:53:29 -0300

    teste commit2

--- app/src/androidTest/java/com/simbora/ApplicationTest.java
+++ app/src/androidTest/java/com/simbora/ApplicationTest.java
@@ -7,12 +7,9 @@ import android.test.ApplicationTestCase;
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
 public class ApplicationTest extends ApplicationTestCase<Application> {
-    protected String testeRepositorio="";
     public ApplicationTest() {
         super(Application.class);
     }
 
-    public String getTesteRepositorio() {
-        return testeRepositorio;
-    }
+
 }
