aad1b4babc65a02565c4c9893e3a418b97dc8150
Krys <c.spinelli93@gmail.com>
2015-06-02 16:25:37 -0300

    PessoaDAO metodo de retornar uma pessoa pelo email feito.

--- app/src/main/java/com/simbora/pessoa/dominio/perssistencia/PessoaDAO.java
+++ app/src/main/java/com/simbora/pessoa/dominio/perssistencia/PessoaDAO.java
@@ -1,13 +1,24 @@
 package com.simbora.pessoa.dominio.perssistencia;
 
+import android.util.Log;
+
 import com.simbora.pessoa.dominio.Pessoa;
 import com.simbora.pessoa.dominio.Usuario;
 import com.simbora.util.dominio.Imagem;
 import com.simbora.util.dominio.Url;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 /**
  * Created by Krys on 02/06/2015.
@@ -49,12 +60,12 @@ public class PessoaDAO {
 
     }
 
-    private Pessoa retornarPessoa(String url){
+    public Pessoa retornarPessoa(String url){
         Pessoa pessoa = new Pessoa();
-        JSONObject pessoaObect = new JSONObject();
+        JSONObject pessoaObect = null;
         try {
-            pessoaObect.get(url);
-            pessoa = retornarPessoa(pessoaObect);
+            pessoaObect = new JSONObject(getJSON(url));
+            pessoa = retornarPessoa(pessoaObect.getJSONObject("pessoa"));
         } catch (JSONException e) {
             e.printStackTrace();
         }
@@ -63,20 +74,21 @@ public class PessoaDAO {
 
     }
 
-    private String retornarUrl(String email){
+    public String retornarUrl(String email){
         String url = "";
         String urlPessoas = "";
         Url urlPagina = new Url();
         urlPessoas = urlPagina.getIp("pessoas");
+        Log.d("Teste", urlPessoas);
 
-        JSONObject jsonObject = new JSONObject();
         try {
-            jsonObject.get(urlPessoas);
+            JSONObject jsonObject = new JSONObject(getJSON(urlPessoas));
             JSONArray pessoas = jsonObject.getJSONArray("pessoas");
             JSONObject usuario = new JSONObject();
             for (int i=0;i<=pessoas.length();i++){
                usuario =  pessoas.getJSONObject(i).getJSONArray("usuario").getJSONObject(0);
-                if(usuario.get("email").equals(email)){
+                Log.d("Teste",usuario.getString("email"));
+                if(usuario.getString("email").equals(email)){
                    url = pessoas.getJSONObject(i).getString("uri");
                 }
             }
@@ -84,6 +96,46 @@ public class PessoaDAO {
             e.printStackTrace();
         }
         return url;
+    }
+
+    //método que retorna a String do JSON
+    private  String getJSON(String url){
+        InputStream inputStream = null;
+        String result = "";
+        try {
+
+            // cria HttpClient
+            HttpClient httpclient = new DefaultHttpClient();
+
+            // dá o GET
+            HttpResponse httpResponse = httpclient.execute(new HttpGet(url));
+
+            // recebe resposta no inputStream
+            inputStream = httpResponse.getEntity().getContent();
+
+            // converte inputStream para string
+            if(inputStream != null)
+                result = convertInputStreamToString(inputStream);
+            else
+                result = "Não funcionou!";
+
+        } catch (Exception e) {
+            Log.d("InputStream", e.getLocalizedMessage());
+        }
+
+        return result;
+    }
+
+    private static String convertInputStreamToString(InputStream inputStream) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader( new InputStreamReader(inputStream));
+        String line = "";
+        String result = "";
+        while((line = bufferedReader.readLine()) != null)
+            result += line;
+
+        inputStream.close();
+        return result;
+
     }
 
 }
