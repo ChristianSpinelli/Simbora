0000dab52fce53ef217d85b03f59b05e6e4e47ea
Renan Azeredo <renanufrpe@gmail.com>
2015-06-03 10:31:21 -0300

    edição Atributos Data da classe Horario

---
+++ app/src/main/java/com/simbora/util/gui/Mask.java
@@ -0,0 +1,113 @@
+package com.simbora.util.gui;
+
+/**
+ * Created by Demis e Lucas on 09/04/2015.
+ */
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+
+import java.util.Calendar;
+
+//classe que tira os caracteres do cpf, data e hora
+public abstract class Mask {
+
+    public static String unmask(String s) {
+        return s.replaceAll("[.]", "").replaceAll("[-]", "")
+                .replaceAll("[/]", "").replaceAll("[(]", "")
+                .replaceAll("[)]", "").replaceAll("[:]", "");
+    }
+
+    public static TextWatcher insert(final String mask, final EditText ediTxt) {
+        return new TextWatcher() {
+            boolean isUpdating;
+            String old = "";
+
+            private String current = "";
+            private String ddmmyyyy = "DDMMYYYY";
+            private Calendar cal = Calendar.getInstance();
+
+            private void mascararData(CharSequence s){
+                if (!s.toString().equals(current)) {
+                    String clean = s.toString().replaceAll("[^\\d.]", "");
+                    String cleanC = current.replaceAll("[^\\d.]", "");
+
+                    int cl = clean.length();
+                    int sel = cl;
+                    for (int i = 2; i <= cl && i < 6; i += 2) {
+                        sel++;
+                    }
+                    //Fix for pressing delete next to a forward slash
+                    if (clean.equals(cleanC)) sel--;
+
+                    if (clean.length() < 8){
+                        clean = clean + ddmmyyyy.substring(clean.length());
+                    }else{
+                        //This part makes sure that when we finish entering numbers
+                        //the date is correct, fixing it otherwise
+                        int day  = Integer.parseInt(clean.substring(0,2));
+                        int mon  = Integer.parseInt(clean.substring(2,4));
+                        int year = Integer.parseInt(clean.substring(4,8));
+
+                        if(mon > 12) mon = 12;
+
+                        cal.set(Calendar.MONTH, mon-1);
+
+                        year = (year<Calendar.YEAR)?Calendar.YEAR:(year>2100)?2100:year;
+                        cal.set(Calendar.YEAR, year);
+                        // ^ first set year for the line below to work correctly
+                        //with leap years - otherwise, date e.g. 29/02/2012
+                        //would be automatically corrected to 28/02/2012
+
+                        day = (day > cal.getActualMaximum(Calendar.DATE))? cal.getActualMaximum(Calendar.DATE):day;
+                        clean = String.format("%02d%02d%02d",day, mon, year);
+                    }
+
+                    clean = String.format("%s/%s/%s", clean.substring(0, 2),
+                            clean.substring(2, 4),
+                            clean.substring(4, 8));
+
+                    sel = sel < 0 ? 0 : sel;
+                    current = clean;
+                    ediTxt.setText(current);
+                    ediTxt.setSelection(sel < current.length() ? sel : current.length());
+                }
+            }
+
+            public void onTextChanged(CharSequence s, int start, int before,
+                                      int count) {
+                String str = Mask.unmask(s.toString());
+                String mascara = "";
+                if (isUpdating) {
+                    old = str;
+                    isUpdating = false;
+                    return;
+                }
+                int i = 0;
+                for (char m : mask.toCharArray()) {
+                    if (m != '#' && str.length() > old.length()) {
+                        mascara += m;
+                        continue;
+                    }
+                    try {
+                        mascara += str.charAt(i);
+                    } catch (Exception e) {
+                        break;
+                    }
+                    i++;
+                }
+                isUpdating = true;
+                ediTxt.setText(mascara);
+                ediTxt.setSelection(mascara.length());
+            }
+
+            public void beforeTextChanged(CharSequence s, int start, int count,
+                                          int after) {
+            }
+
+            public void afterTextChanged(Editable s) {
+            }
+        };
+    }
+
+}
